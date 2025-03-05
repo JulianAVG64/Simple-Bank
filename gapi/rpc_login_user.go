d60ae69f3a10340fa3e85a929461ca86be31bd93
JulianAVG64 <julianavg2002@gmail.com>
2025-03-05 14:06:24 -0500

    validate gRPC parameters, human/machine friendly error response

--- gapi/rpc_login_user.go
+++ gapi/rpc_login_user.go
@@ -7,12 +7,20 @@ import (
 	db "github.com/JulianAVG64/Simple-Bank/db/sqlc"
 	"github.com/JulianAVG64/Simple-Bank/pb"
 	"github.com/JulianAVG64/Simple-Bank/util"
+	"github.com/JulianAVG64/Simple-Bank/val"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (*pb.LoginUserResponse, error) {
+	// Error handling
+	violations := validateLoginUserRequest(req)
+	if violations != nil {
+		return nil, invalidArgumentError(violations)
+	}
+
 	// Check if user exist
 	user, err := server.store.GetUser(ctx, req.GetUsername())
 	if err != nil {
@@ -69,4 +77,16 @@ func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (
 		RefreshTokenExpiresAt: timestamppb.New(refreshPayload.ExpiredAt),
 	}
 	return rsp, nil
+}
+
+func validateLoginUserRequest(req *pb.LoginUserRequest) (violations []*errdetails.BadRequest_FieldViolation) {
+	if err := val.ValidateUsername(req.GetUsername()); err != nil {
+		violations = append(violations, fieldViolation("username", err))
+	}
+
+	if err := val.ValidatePassword(req.GetPassword()); err != nil {
+		violations = append(violations, fieldViolation("password", err))
+	}
+
+	return violations
 }
--- go.mod
+++ go.mod
@@ -26,7 +26,7 @@ require (
 	github.com/aead/chacha20poly1305 v0.0.0-20170617001512-233f39982aeb // indirect
 	github.com/aead/poly1305 v0.0.0-20180717145839-3fee0db0b635 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20250204164813-702378808489 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250204164813-702378808489
 )
 
 require (
