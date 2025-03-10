96bac285c7019917394ac869159b9536dbe8442d
JulianAVG64 <julianavg2002@gmail.com>
2025-03-10 14:03:13 -0500

    handle errors and print logs for Async workers

--- worker/processor.go
+++ worker/processor.go
@@ -6,6 +6,7 @@ import (
 
 	db "github.com/JulianAVG64/Simple-Bank/db/sqlc"
 	"github.com/hibiken/asynq"
+	"github.com/rs/zerolog/log"
 )
 
 const (
@@ -31,6 +32,10 @@ func NewRedisTaskProcessor(redisOpt asynq.RedisClientOpt, store db.Store) TaskPr
 				QueueCritical: 10,
 				QueueDefault:  5,
 			},
+			ErrorHandler: asynq.ErrorHandlerFunc(func(ctx context.Context, task *asynq.Task, err error) {
+				log.Error().Err(err).Str("type", task.Type()).Bytes("payload", task.Payload()).Msg("process task failed")
+			}),
+			Logger: NewLogger(),
 		},
 	)
 
